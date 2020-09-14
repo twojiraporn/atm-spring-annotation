4af47c9c53e8e46aba02327c0f9013d95c078d68
twojiraporn <jiraporn.kow>
2020-09-14 18:48:08 +0700

    first commit

---
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -0,0 +1,17 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean.xml");
+
+        ATM atm = context.getBean(ATM.class);
+
+        AtmUI atmUI = new AtmUI(atm);
+        atmUI.run();
+    }
+}
